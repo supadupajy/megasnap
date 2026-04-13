fe1529c3e45642bb7c8260464c797270ffbc2e05
[dyad] <supadupajy@gmail.com>
2026-04-13 22:31:32 +0900

    [dyad] 상세 카드 인기 포스팅 테두리 두께를 광고와 동일하게 조정 - wrote 2 file(s)

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -250,10 +250,10 @@ const PostDetail = ({ posts, initialIndex, isOpen, onClose, onViewPost, onLikeTo
                   "absolute pointer-events-auto w-[90vw] sm:max-w-[420px] rounded-[40px] overflow-hidden shadow-[0_50px_100px_-20px_rgba(0,0,0,0.7)] flex flex-col h-[82vh] will-change-transform bg-white",
                   isInfluencer && "influencer-border-container animate-influencer-float",
                   isPopular && "popular-border-container",
-                  isAd && "p-[3px] bg-blue-500"
+                  isAd && "p-[4px] bg-blue-500"
                 )}
               >
-                <div className="flex-1 h-full overflow-hidden flex flex-col bg-white rounded-[37px]">
+                <div className="flex-1 h-full overflow-hidden flex flex-col bg-white rounded-[36px]">
                   <div 
                     key={`scroll-container-${post.id}`}
                     ref={scrollContainerRef} 
