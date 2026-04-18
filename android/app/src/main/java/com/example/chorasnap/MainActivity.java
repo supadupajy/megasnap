0b6eb8f4787d617176891bf16e35acea7aa659dc
[dyad] <supadupajy@gmail.com>
2026-04-18 19:56:15 +0900

    Reverted all changes back to version afb2f34a996d83bd177462fc4a7fe03fe632dd3a

--- android/app/src/main/java/com/example/chorasnap/MainActivity.java
+++ android/app/src/main/java/com/example/chorasnap/MainActivity.java
@@ -1,20 +1,5 @@
 package com.example.chorasnap;
 
-import android.os.Bundle;
-import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // 웹뷰에서 사용자 조작 없이도 소리가 포함된 미디어 자동 재생을 허용합니다.
-        WebSettings settings = this.bridge.getWebView().getSettings();
-        settings.setMediaPlaybackRequiresUserGesture(false);
-    }
-}
+public class MainActivity extends BridgeActivity {}
