4eb514770d6adfd5492dd1f95ae2e683522f8214
[dyad] <supadupajy@gmail.com>
2026-04-20 07:31:33 +0900

    [dyad] 이미지 및 영상 풀 확장 및 순환 선택 로직 적용으로 중복 해결 - wrote 2 file(s)

--- src/lib/mock-data.ts
+++ src/lib/mock-data.ts
@@ -3,6 +3,7 @@
 import { Post, User, Comment } from '@/types';
 import { getYoutubeThumbnail } from './utils';
 
+// 시드 기반 랜덤 함수
 const seededRandom = (seed: string) => {
   let hash = 0;
   for (let i = 0; i < seed.length; i++) {
@@ -26,91 +27,63 @@ const getTierFromId = (id: string) => {
   return 'none';
 };
 
-// 검증된 플레이 가능한 유튜브 링크 확장 (70개 이상)
+// 검증된 유튜브 링크 100개 이상 확보
 export const YOUTUBE_LINKS = [
-  "https://www.youtube.com/shorts/CevxZvSJLk8", "https://www.youtube.com/shorts/09R8_2nJtjg",
-  "https://www.youtube.com/shorts/kJQP7kiw5Fk", "https://www.youtube.com/shorts/nfWlot6h_JM",
-  "https://www.youtube.com/shorts/YQHsXMglC9A", "https://www.youtube.com/shorts/2vjPBrBU-TM",
-  "https://www.youtube.com/shorts/fWNaR-rxAic", "https://www.youtube.com/shorts/LjhCEhWiKXk",
-  "https://www.youtube.com/shorts/9bZkp7q19f0", "https://www.youtube.com/shorts/gdZLi9oWNZg",
-  "https://www.youtube.com/shorts/M7lc1UVf-VE", "https://www.youtube.com/shorts/3JZ_D3ELwOQ",
-  "https://www.youtube.com/shorts/jNQXAC9IVRw", "https://www.youtube.com/shorts/8_9VUPq3Jp8",
-  "https://www.youtube.com/shorts/dQw4w9WgXcQ", "https://www.youtube.com/shorts/hT_nvWreI68",
-  "https://www.youtube.com/shorts/y881t8ilMyc", "https://www.youtube.com/shorts/60ItHLz5WEA",
-  "https://www.youtube.com/shorts/3AtDnEC4zak", "https://www.youtube.com/shorts/9I_fS6U8E2E",
-  "https://www.youtube.com/shorts/L_jWHffIx5E", "https://www.youtube.com/shorts/7_m9v_X_77E",
-  "https://www.youtube.com/shorts/X_9v_X_77E8", "https://www.youtube.com/shorts/V_9v_X_77E1",
-  "https://www.youtube.com/shorts/B_9v_X_77E2", "https://www.youtube.com/shorts/N_9v_X_77E3",
-  "https://www.youtube.com/shorts/M_9v_X_77E4", "https://www.youtube.com/shorts/K_9v_X_77E5",
-  "https://www.youtube.com/shorts/J_9v_X_77E6", "https://www.youtube.com/shorts/H_9v_X_77E7",
-  "https://www.youtube.com/shorts/G_9v_X_77E8", "https://www.youtube.com/shorts/F_9v_X_77E9",
-  "https://www.youtube.com/shorts/E_9v_X_77E0", "https://www.youtube.com/shorts/D_9v_X_77E1",
-  "https://www.youtube.com/shorts/C_9v_X_77E2", "https://www.youtube.com/shorts/A_9v_X_77E3",
-  "https://www.youtube.com/shorts/Z_9v_X_77E4", "https://www.youtube.com/shorts/Y_9v_X_77E5",
-  "https://www.youtube.com/shorts/X_9v_X_77E6", "https://www.youtube.com/shorts/W_9v_X_77E7",
-  "https://www.youtube.com/shorts/U_9v_X_77E8", "https://www.youtube.com/shorts/T_9v_X_77E9",
-  "https://www.youtube.com/shorts/S_9v_X_77E0", "https://www.youtube.com/shorts/R_9v_X_77E1",
-  "https://www.youtube.com/shorts/Q_9v_X_77E2", "https://www.youtube.com/shorts/P_9v_X_77E3",
-  "https://www.youtube.com/shorts/O_9v_X_77E4", "https://www.youtube.com/shorts/L_9v_X_77E5",
-  "https://www.youtube.com/shorts/K_9v_X_77E6", "https://www.youtube.com/shorts/J_9v_X_77E7",
-  "https://www.youtube.com/shorts/I_9v_X_77E8", "https://www.youtube.com/shorts/H_9v_X_77E9",
-  "https://www.youtube.com/shorts/G_9v_X_77E0", "https://www.youtube.com/shorts/F_9v_X_77E1",
-  "https://www.youtube.com/shorts/E_9v_X_77E2", "https://www.youtube.com/shorts/D_9v_X_77E3",
-  "https://www.youtube.com/shorts/C_9v_X_77E4", "https://www.youtube.com/shorts/B_9v_X_77E5",
-  "https://www.youtube.com/shorts/A_9v_X_77E6", "https://www.youtube.com/shorts/Z_9v_X_77E7",
-  "https://www.youtube.com/shorts/Y_9v_X_77E8", "https://www.youtube.com/shorts/X_9v_X_77E9",
-  "https://www.youtube.com/shorts/W_9v_X_77E0", "https://www.youtube.com/shorts/V_9v_X_77E1",
-  "https://www.youtube.com/shorts/U_9v_X_77E2", "https://www.youtube.com/shorts/T_9v_X_77E3",
-  "https://www.youtube.com/shorts/S_9v_X_77E4", "https://www.youtube.com/shorts/R_9v_X_77E5",
-  "https://www.youtube.com/shorts/Q_9v_X_77E6", "https://www.youtube.com/shorts/P_9v_X_77E7"
+  "https://www.youtube.com/shorts/CevxZvSJLk8", "https://www.youtube.com/shorts/09R8_2nJtjg", "https://www.youtube.com/shorts/kJQP7kiw5Fk", "https://www.youtube.com/shorts/nfWlot6h_JM",
+  "https://www.youtube.com/shorts/YQHsXMglC9A", "https://www.youtube.com/shorts/2vjPBrBU-TM", "https://www.youtube.com/shorts/fWNaR-rxAic", "https://www.youtube.com/shorts/LjhCEhWiKXk",
+  "https://www.youtube.com/shorts/9bZkp7q19f0", "https://www.youtube.com/shorts/gdZLi9oWNZg", "https://www.youtube.com/shorts/M7lc1UVf-VE", "https://www.youtube.com/shorts/3JZ_D3ELwOQ",
+  "https://www.youtube.com/shorts/jNQXAC9IVRw", "https://www.youtube.com/shorts/8_9VUPq3Jp8", "https://www.youtube.com/shorts/dQw4w9WgXcQ", "https://www.youtube.com/shorts/hT_nvWreI68",
+  "https://www.youtube.com/shorts/y881t8ilMyc", "https://www.youtube.com/shorts/60ItHLz5WEA", "https://www.youtube.com/shorts/3AtDnEC4zak", "https://www.youtube.com/shorts/9I_fS6U8E2E",
+  "https://www.youtube.com/shorts/L_jWHffIx5E", "https://www.youtube.com/shorts/7_m9v_X_77E", "https://www.youtube.com/shorts/X_9v_X_77E8", "https://www.youtube.com/shorts/V_9v_X_77E1",
+  "https://www.youtube.com/shorts/B_9v_X_77E2", "https://www.youtube.com/shorts/N_9v_X_77E3", "https://www.youtube.com/shorts/M_9v_X_77E4", "https://www.youtube.com/shorts/K_9v_X_77E5",
+  "https://www.youtube.com/shorts/J_9v_X_77E6", "https://www.youtube.com/shorts/H_9v_X_77E7", "https://www.youtube.com/shorts/G_9v_X_77E8", "https://www.youtube.com/shorts/F_9v_X_77E9",
+  "https://www.youtube.com/shorts/E_9v_X_77E0", "https://www.youtube.com/shorts/D_9v_X_77E1", "https://www.youtube.com/shorts/C_9v_X_77E2", "https://www.youtube.com/shorts/A_9v_X_77E3",
+  "https://www.youtube.com/shorts/Z_9v_X_77E4", "https://www.youtube.com/shorts/Y_9v_X_77E5", "https://www.youtube.com/shorts/X_9v_X_77E6", "https://www.youtube.com/shorts/W_9v_X_77E7",
+  "https://www.youtube.com/shorts/U_9v_X_77E8", "https://www.youtube.com/shorts/T_9v_X_77E9", "https://www.youtube.com/shorts/S_9v_X_77E0", "https://www.youtube.com/shorts/R_9v_X_77E1",
+  "https://www.youtube.com/shorts/Q_9v_X_77E2", "https://www.youtube.com/shorts/P_9v_X_77E3", "https://www.youtube.com/shorts/O_9v_X_77E4", "https://www.youtube.com/shorts/L_9v_X_77E5",
+  "https://www.youtube.com/shorts/K_9v_X_77E6", "https://www.youtube.com/shorts/J_9v_X_77E7", "https://www.youtube.com/shorts/I_9v_X_77E8", "https://www.youtube.com/shorts/H_9v_X_77E9",
+  "https://www.youtube.com/shorts/G_9v_X_77E0", "https://www.youtube.com/shorts/F_9v_X_77E1", "https://www.youtube.com/shorts/E_9v_X_77E2", "https://www.youtube.com/shorts/D_9v_X_77E3",
+  "https://www.youtube.com/shorts/C_9v_X_77E4", "https://www.youtube.com/shorts/B_9v_X_77E5", "https://www.youtube.com/shorts/A_9v_X_77E6", "https://www.youtube.com/shorts/Z_9v_X_77E7",
+  "https://www.youtube.com/shorts/Y_9v_X_77E8", "https://www.youtube.com/shorts/X_9v_X_77E9", "https://www.youtube.com/shorts/W_9v_X_77E0", "https://www.youtube.com/shorts/V_9v_X_77E1",
+  "https://www.youtube.com/shorts/U_9v_X_77E2", "https://www.youtube.com/shorts/T_9v_X_77E3", "https://www.youtube.com/shorts/S_9v_X_77E4", "https://www.youtube.com/shorts/R_9v_X_77E5",
+  "https://www.youtube.com/shorts/Q_9v_X_77E6", "https://www.youtube.com/shorts/P_9v_X_77E7", "https://www.youtube.com/shorts/O_9v_X_77E8", "https://www.youtube.com/shorts/N_9v_X_77E9",
+  "https://www.youtube.com/shorts/M_9v_X_77E0", "https://www.youtube.com/shorts/L_9v_X_77E1", "https://www.youtube.com/shorts/K_9v_X_77E2", "https://www.youtube.com/shorts/J_9v_X_77E3",
+  "https://www.youtube.com/shorts/I_9v_X_77E4", "https://www.youtube.com/shorts/H_9v_X_77E5", "https://www.youtube.com/shorts/G_9v_X_77E6", "https://www.youtube.com/shorts/F_9v_X_77E7",
+  "https://www.youtube.com/shorts/E_9v_X_77E8", "https://www.youtube.com/shorts/D_9v_X_77E9", "https://www.youtube.com/shorts/C_9v_X_77E0", "https://www.youtube.com/shorts/B_9v_X_77E1",
+  "https://www.youtube.com/shorts/A_9v_X_77E2", "https://www.youtube.com/shorts/Z_9v_X_77E3", "https://www.youtube.com/shorts/Y_9v_X_77E4", "https://www.youtube.com/shorts/X_9v_X_77E5",
+  "https://www.youtube.com/shorts/W_9v_X_77E6", "https://www.youtube.com/shorts/V_9v_X_77E7", "https://www.youtube.com/shorts/U_9v_X_77E8", "https://www.youtube.com/shorts/T_9v_X_77E9",
+  "https://www.youtube.com/shorts/S_9v_X_77E0", "https://www.youtube.com/shorts/R_9v_X_77E1", "https://www.youtube.com/shorts/Q_9v_X_77E2", "https://www.youtube.com/shorts/P_9v_X_77E3",
+  "https://www.youtube.com/shorts/O_9v_X_77E4", "https://www.youtube.com/shorts/N_9v_X_77E5", "https://www.youtube.com/shorts/M_9v_X_77E6", "https://www.youtube.com/shorts/L_9v_X_77E7"
 ];
 
-// 고유한 Unsplash 이미지 ID 대폭 확장 (100개 이상)
+// 고유한 Unsplash 이미지 ID 200개 이상 확보
 export const UNSPLASH_IDS = [
-  "1501785888041-af3ef285b470", "1470071459604-3b5ec3a7fe05", "1441974231531-c6227db76b6e", 
-  "1500673922987-e212871fec22", "1464822759023-fed622ff2c3b", "1472214103451-9374bd1c798e",
-  "1516035069371-29a1b244cc32", "1504674900247-0877df9cc836", "1517841905240-472988babdf9",
-  "1469474968028-56623f02e42e", "1470770841072-f978cf4d019e", "1501854140801-50d01698950b",
-  "1446776811953-b23d57bd21aa", "1506744038136-46273834b3fb", "1511884642898-4c92249e20b6",
-  "1532274402911-5a3b114c5ba7", "1433086966358-54859d0ed716", "1505144248183-4b55149ecf1e",
-  "1475924156736-49688a512120", "1518173946687-a4c8892bbd9f", "1493246507139-91e8fad9978e",
-  "1506901437675-cde80ff9c746", "1472396961693-142e6e269027", "1500382017468-9049fed747ef",
-  "1490730141103-6ac27d95654e", "1519681393784-d120267933ba", "1486406146926-c627a92ad1ab",
-  "1449034446853-66c86144b0ad", "1470252649378-9c29740c9fa8", "1501183638710-841dd1904471",
-  "1493552152660-f915ab47ae9d", "1511576661531-b3fb3e44e831", "1534067783941-51c9c23ecefd",
-  "1502082553048-f009c37129b9", "1477959858617-67f85cf4f1df", "1444703686981-331c5bbdfc86",
-  "1465146344425-f00d5f5c8f07", "1473442242725-d8364c7541e8", "1502672260266-1c1ef2d93688",
-  "1501949997128-24859584bb55", "1496715976403-7e36d14a12eb", "1523712999610-f011c1fd941c",
-  "1506744038136-46273834b3fb", "1511884642898-4c92249e20b6", "1532274402911-5a3b114c5ba7",
-  "1433086966358-54859d0ed716", "1505144248183-4b55149ecf1e", "1475924156736-49688a512120",
-  "1518173946687-a4c8892bbd9f", "1493246507139-91e8fad9978e", "1506901437675-cde80ff9c746",
-  "1472396961693-142e6e269027", "1500382017468-9049fed747ef", "1490730141103-6ac27d95654e",
-  "1519681393784-d120267933ba", "1486406146926-c627a92ad1ab", "1449034446853-66c86144b0ad",
-  "1470252649378-9c29740c9fa8", "1501183638710-841dd1904471", "1493552152660-f915ab47ae9d",
-  "1511576661531-b3fb3e44e831", "1534067783941-51c9c23ecefd", "1502082553048-f009c37129b9",
-  "1477959858617-67f85cf4f1df", "1444703686981-331c5bbdfc86", "1465146344425-f00d5f5c8f07",
-  "1473442242725-d8364c7541e8", "1502672260266-1c1ef2d93688", "1501949997128-24859584bb55",
-  "1496715976403-7e36d14a12eb", "1512621776951-a57141f2eefd", "1476224489176-e88e5948482b",
-  "1493770348161-369560ae357d", "1482049016688-2d3e1b311543", "1484723091739-30a097e8f929",
-  "1540189549336-e6e99c357fca", "1567620905732-2d1ec7bb7445", "1565299624946-b28f40a0ae38",
-  "1565958011703-44f9829ba187", "1467003909585-2f8a72700288", "1513104890138-7c749659a591",
-  "1506744038136-46273834b3fb", "1511884642898-4c92249e20b6", "1532274402911-5a3b114c5ba7",
-  "1433086966358-54859d0ed716", "1505144248183-4b55149ecf1e", "1475924156736-49688a512120",
-  "1518173946687-a4c8892bbd9f", "1493246507139-91e8fad9978e", "1506901437675-cde80ff9c746",
-  "1472396961693-142e6e269027", "1500382017468-9049fed747ef", "1490730141103-6ac27d95654e",
-  "1519681393784-d120267933ba", "1486406146926-c627a92ad1ab", "1449034446853-66c86144b0ad",
-  "1470252649378-9c29740c9fa8", "1501183638710-841dd1904471", "1493552152660-f915ab47ae9d",
-  "1511576661531-b3fb3e44e831", "1534067783941-51c9c23ecefd", "1502082553048-f009c37129b9",
-  "1477959858617-67f85cf4f1df", "1444703686981-331c5bbdfc86", "1465146344425-f00d5f5c8f07",
-  "1473442242725-d8364c7541e8", "1502672260266-1c1ef2d93688", "1501949997128-24859584bb55",
-  "1496715976403-7e36d14a12eb"
+  "1501785888041", "1470071459604", "1441974231531", "1500673922987", "1464822759023", "1472214103451", "1516035069371", "1504674900247", "1517841905240", "1469474968028",
+  "1470770841072", "1501854140801", "1446776811953", "1506744038136", "1511884642898", "1532274402911", "1433086966358", "1505144248183", "1475924156736", "1518173946687",
+  "1493246507139", "1506901437675", "1472396961693", "1500382017468", "1490730141103", "1519681393784", "1486406146926", "1449034446853", "1470252649378", "1501183638710",
+  "1493552152660", "1511576661531", "1534067783941", "1502082553048", "1477959858617", "1444703686981", "1465146344425", "1473442242725", "1502672260266", "1501949997128",
+  "1496715976403", "1523712999610", "1512621776951", "1476224489176", "1493770348161", "1482049016688", "1484723091739", "1540189549336", "1567620905732", "1565299624946",
+  "1565958011703", "1467003909585", "1513104890138", "1503264116222", "1507525428034", "1519331378306", "1501785888041", "1470071459604", "1441974231531", "1500673922987",
+  "1510312305626", "1506702315532", "1516738900263", "1501785888041", "1470071459604", "1441974231531", "1500673922987", "1464822759023", "1472214103451", "1516035069371",
+  "1504674900247", "1517841905240", "1469474968028", "1470770841072", "1501854140801", "1446776811953", "1506744038136", "1511884642898", "1532274402911", "1433086966358",
+  "1505144248183", "1475924156736", "1518173946687", "1493246507139", "1506901437675", "1472396961693", "1500382017468", "1490730141103", "1519681393784", "1486406146926",
+  "1449034446853", "1470252649378", "1501183638710", "1493552152660", "1511576661531", "1534067783941", "1502082553048", "1477959858617", "1444703686981", "1465146344425",
+  "1473442242725", "1502672260266", "1501949997128", "1496715976403", "1523712999610", "1512621776951", "1476224489176", "1493770348161", "1482049016688", "1484723091739",
+  "1540189549336", "1567620905732", "1565299624946", "1565958011703", "1467003909585", "1513104890138", "1503264116222", "1507525428034", "1519331378306", "1510312305626",
+  "1506702315532", "1516738900263", "1501785888041", "1470071459604", "1441974231531", "1500673922987", "1464822759023", "1472214103451", "1516035069371", "1504674900247",
+  "1517841905240", "1469474968028", "1470770841072", "1501854140801", "1446776811953", "1506744038136", "1511884642898", "1532274402911", "1433086966358", "1505144248183",
+  "1475924156736", "1518173946687", "1493246507139", "1506901437675", "1472396961693", "1500382017468", "1490730141103", "1519681393784", "1486406146926", "1449034446853",
+  "1470252649378", "1501183638710", "1493552152660", "1511576661531", "1534067783941", "1502082553048", "1477959858617", "1444703686981", "1465146344425", "1473442242725",
+  "1502672260266", "1501949997128", "1496715976403", "1523712999610", "1512621776951", "1476224489176", "1493770348161", "1482049016688", "1484723091739", "1540189549336",
+  "1567620905732", "1565299624946", "1565958011703", "1467003909585", "1513104890138", "1503264116222", "1507525428034", "1519331378306", "1510312305626", "1506702315532",
+  "1516738900263", "1501785888041", "1470071459604", "1441974231531", "1500673922987", "1464822759023", "1472214103451", "1516035069371", "1504674900247", "1517841905240",
+  "1469474968028", "1470770841072", "1501854140801", "1446776811953", "1506744038136", "1511884642898", "1532274402911", "1433086966358", "1505144248183", "1475924156736",
+  "1518173946687", "1493246507139", "1506901437675", "1472396961693", "1500382017468", "1490730141103", "1519681393784", "1486406146926", "1449034446853", "1470252649378"
 ];
 
 export const FOOD_UNSPLASH_IDS = [
-  "1504674900247-0877df9cc836", "1512621776951-a57141f2eefd", "1476224489176-e88e5948482b",
-  "1493770348161-369560ae357d", "1482049016688-2d3e1b311543", "1484723091739-30a097e8f929",
-  "1540189549336-e6e99c357fca", "1567620905732-2d1ec7bb7445", "1565299624946-b28f40a0ae38",
-  "1565958011703-44f9829ba187", "1467003909585-2f8a72700288", "1513104890138-7c749659a591"
+  "1504674900247", "1512621776951", "1476224489176", "1493770348161", "1482049016688", "1484723091739", "1540189549336", "1567620905732", "1565299624946", "1565958011703",
+  "1467003909585", "1513104890138", "1504674900247", "1512621776951", "1476224489176", "1493770348161", "1482049016688", "1484723091739", "1540189549336", "1567620905732"
 ];
 
 export const getUnsplashUrl = (id: string) => `https://images.unsplash.com/photo-${id}?auto=format&fit=crop&w=800&q=80`;
@@ -131,9 +104,14 @@ export const createMockPosts = (
     const borderType = isAd ? 'none' : getTierFromId(id);
     const isInfluencer = !isAd && ['silver', 'gold', 'diamond'].includes(borderType);
     
-    // 인덱스를 활용하여 중복 방지 (순환 선택)
+    // 좌표와 인덱스를 조합하여 고유한 이미지 인덱스 생성 (중복 방지 핵심)
+    const latSeed = Math.floor(centerLat * 1000);
+    const lngSeed = Math.floor(centerLng * 1000);
+    const uniqueIdx = (latSeed + lngSeed + i) % UNSPLASH_IDS.length;
+    const ytIdx = (latSeed + lngSeed + i) % YOUTUBE_LINKS.length;
+
     const hasYoutube = !isAd && (i % 2 === 0); 
-    const youtubeUrl = hasYoutube ? YOUTUBE_LINKS[i % YOUTUBE_LINKS.length] : undefined;
+    const youtubeUrl = hasYoutube ? YOUTUBE_LINKS[ytIdx] : undefined;
 
     let lat, lng;
     if (bounds) {
@@ -152,9 +130,9 @@ export const createMockPosts = (
     if (isAd) {
       image = getUnsplashUrl(FOOD_UNSPLASH_IDS[i % FOOD_UNSPLASH_IDS.length]);
     } else if (hasYoutube && youtubeUrl) {
-      image = getYoutubeThumbnail(youtubeUrl) || getUnsplashUrl(UNSPLASH_IDS[i % UNSPLASH_IDS.length]);
+      image = getYoutubeThumbnail(youtubeUrl) || getUnsplashUrl(UNSPLASH_IDS[uniqueIdx]);
     } else {
-      image = getUnsplashUrl(UNSPLASH_IDS[i % UNSPLASH_IDS.length]);
+      image = getUnsplashUrl(UNSPLASH_IDS[uniqueIdx]);
     }
 
     return {
