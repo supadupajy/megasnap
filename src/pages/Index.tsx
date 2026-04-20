436dd9a6fbb1879d8928264799de4bf3d6ad5132
[dyad] <supadupajy@gmail.com>
2026-04-20 22:12:13 +0900

    Updated src/pages/Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -380,7 +380,7 @@ const Index = () => {
                 </button>
                 <div className="relative">
                   {displayedMarkers.length > 0 && currentZoom < 11 && (
-                    <div className="absolute inset-0 -m-1 bg-indigo-400/30 rounded-[30px] animate-ping pointer-events-none" />
+                    <div className="absolute inset-1 -m-1 bg-indigo-400/30 rounded-[30px] animate-ping pointer-events-none" />
                   )}
                   <button
                     onClick={handleViewAllClick}
