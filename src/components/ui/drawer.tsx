b0f8d620b29bab1f3ff7deffad51278fc593a6e7
[dyad] <supadupajy@gmail.com>
2026-04-20 19:05:35 +0900

    [dyad] 글쓰기 팝업이 하단 메뉴를 가리지 않도록 z-index 설정 수정 - wrote 3 file(s)

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import { Drawer as DrawerPrimitive } from "vaul";
 
@@ -26,7 +28,8 @@ const DrawerOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Overlay
     ref={ref}
-    className={cn("fixed inset-0 z-50 bg-black/80", className)}
+    // z-index를 1000으로 설정하여 BottomNav (z-1002) 아래에 위치하도록 함
+    className={cn("fixed inset-0 z-[1000] bg-black/80", className)}
     {...props}
   />
 ));
@@ -41,7 +44,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
+        "fixed inset-x-0 bottom-0 z-[1001] mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
         className,
       )}
       {...props}
