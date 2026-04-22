ecce35b3ecf5fd734ca40996614f2ddff378b418
[dyad] <supadupajy@gmail.com>
2026-04-22 17:24:04 +0900

    [dyad] 영상 포스팅 열람 시 음소거 없이 소리가 함께 재생되도록 설정을 변경했습니다. 유튜브 및 직접 업로드 영상 모두 소리가 활성화된 상태로 자동 재생되며, 사용자 편의를 위해 컨트롤바를 추가했습니다.

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -400,7 +400,7 @@ const PostDetail = ({ posts, initialIndex, isOpen, onClose, onViewPost, onLikeTo
                         <div className="w-full h-full relative">
                           <iframe
                             className="w-full h-full object-cover"
-                            src={`https://www.youtube.com/embed/${youtubeId}?autoplay=1&mute=1&controls=0&loop=1&playlist=${youtubeId}`}
+                            src={`https://www.youtube.com/embed/${youtubeId}?autoplay=1&mute=0&controls=1&loop=1&playlist=${youtubeId}`}
                             title="YouTube video player"
                             frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -413,9 +413,9 @@ const PostDetail = ({ posts, initialIndex, isOpen, onClose, onViewPost, onLikeTo
                             src={post.videoUrl}
                             className="w-full h-full object-cover"
                             autoPlay
-                            muted
                             loop
                             playsInline
+                            controls
                           />
                         </div>
                       ) : (
