dd9788f8d1d1dcd54d872b2d0b767080f18836d6
[dyad] <supadupajy@gmail.com>
2026-04-23 00:36:52 +0900

    [dyad] 자동 재생되는 유튜브 영상의 무음 설정을 해제하고 소리가 나오도록 수정했습니다. 브라우저 정책 준수를 위해 컨트롤바도 활성화했습니다.

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -201,7 +201,7 @@ const PostItem = ({ post, onLikeToggle, onLocationClick, onDelete, onSaveToggle,
         {videoId ? (
           <div className="w-full h-full">
             <iframe
-              src={`https://www.youtube.com/embed/${videoId}?autoplay=${autoPlayVideo ? 1 : 0}&mute=1&loop=1&playlist=${videoId}&controls=0&modestbranding=1&rel=0&showinfo=0`}
+              src={`https://www.youtube.com/embed/${videoId}?autoplay=${autoPlayVideo ? 1 : 0}&mute=0&loop=1&playlist=${videoId}&controls=1&modestbranding=1&rel=0&showinfo=0`}
               className="w-full h-full object-cover"
               allow="autoplay; encrypted-media"
               allowFullScreen
