8277d73a9c01c73e4194c14c261b396a50a03d8e
[dyad] <supadupajy@gmail.com>
2026-04-23 16:05:14 +0900

    Updated src/components/BottomNav.tsx

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -21,7 +21,7 @@ const BottomNav = () => {
   const isActive = (path: string) => location.pathname === path;
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 h-[100px] bg-white border-t border-gray-100 flex items-center justify-around px-2 z-[1000] pb-6">
+    <nav className="fixed bottom-0 left-0 right-0 h-[100px] bg-white border-t border-gray-100 flex items-center justify-around px-2 z-[0] pb-6">
       <button 
         onClick={() => navigate('/')}
         className={cn(
