c53c24395079ffbc149d9f31035eb2c3e02e56a5
[dyad] <supadupajy@gmail.com>
2026-04-26 22:56:51 +0900

    Updated src/components/PostDetail.tsx

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -416,7 +416,7 @@ const PostDetail = ({ posts, initialIndex, isOpen, onClose, onDelete, onViewPost
           
           <div className="relative flex-1 flex flex-col min-h-0">
             {/* Header */}
-            <div className="absolute top-0 left-0 right-0 z-50 flex items-start justify-end px-4 pt-2 pointer-events-none">
+            <div className="absolute top-0 left-0 right-0 z-50 flex items-start justify-end px-4 pt-3 pointer-events-none">
               <div className="flex items-center gap-2 pointer-events-auto">
                 <button
                   onClick={onClose}
@@ -576,7 +576,7 @@ const PostDetail = ({ posts, initialIndex, isOpen, onClose, onDelete, onViewPost
                       </div>
 
                       {/* 액션 버튼 및 내용 - 아이콘 그룹과 버튼 그룹 분리 */}
-                      <div className="px-4 pt-3 pb-4" onClick={(e) => e.stopPropagation()} >
+                      <div className="px-4 pt-2 pb-4" onClick={(e) => e.stopPropagation()} >
                         <div className="flex flex-col gap-3">
                           {/* 상단: 아이콘 그룹과 기본 뱃지/위치보기 */}
                           <div className="flex items-center justify-between">
