0f62ad80f1aa507aa9a642907375360c0f5d5980
[dyad] <supadupajy@gmail.com>
2026-04-29 18:20:08 +0900

    [dyad] 앱 전체 폰트를 LINE Seed Sans KR로 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ export default {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["'LINE Seed KR'", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
