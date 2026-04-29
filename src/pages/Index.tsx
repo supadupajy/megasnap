a916497e067a7ca41cfc9d680f6615d077f08d50
[dyad] <supadupajy@gmail.com>
2026-04-29 22:41:36 +0900

    [dyad] 모두보기 셔터 애니메이션을 코너 브라켓 대신 인디고 테두리 + 흰색 플래시로 변경

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -831,13 +831,10 @@ const Index = () => {
           {/* 모두보기 카메라 셔터 애니메이션 - 지도 영역 안에만 표시 */}
           {shutterActive && (
             <div className="absolute inset-0 pointer-events-none" style={{ zIndex: 25 }}>
-              {/* 플래시 오버레이 - 코너 브라켓 아래 */}
+              {/* 플래시 오버레이 */}
               <div className="shutter-flash" style={{ position: 'absolute', inset: 0, zIndex: 1 }} />
-              {/* 네 모서리 코너 브라켓 - 플래시 위 */}
-              <div className="shutter-corner shutter-corner-tl" style={{ zIndex: 2 }} />
-              <div className="shutter-corner shutter-corner-tr" style={{ zIndex: 2 }} />
-              <div className="shutter-corner shutter-corner-bl" style={{ zIndex: 2 }} />
-              <div className="shutter-corner shutter-corner-br" style={{ zIndex: 2 }} />
+              {/* 인디고 테두리 */}
+              <div className="shutter-border" style={{ position: 'absolute', inset: 0, zIndex: 2 }} />
             </div>
           )}
           <div className="absolute inset-0 z-0">
