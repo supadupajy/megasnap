3e43c9ae5d371a6e1a39b93aeeaf468a29a16590
[dyad] <supadupajy@gmail.com>
2026-05-07 20:40:54 +0900

    Updated src/pages/VideoSearch.tsx

--- src/pages/VideoSearch.tsx
+++ src/pages/VideoSearch.tsx
@@ -307,7 +307,7 @@ const VideoSearch = () => {
           <div className="relative">
             <SearchIcon className="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 text-indigo-600 z-10" />
             <input
-              placeholder="원하는 포스팅을 검색해 보세요"
+              placeholder="원하는 포스팅요"
               className="w-full pl-12 h-14 bg-white border-2 border-indigo-600 rounded-2xl outline-none font-semibold placeholder:text-gray-400 placeholder:font-semibold shadow-sm transition-all focus:ring-2 focus:ring-indigo-50"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
