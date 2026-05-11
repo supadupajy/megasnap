33c2433ae60eccd456b688355ab03b51acf5516f
[dyad] <supadupajy@gmail.com>
2026-05-11 20:21:38 +0900

    [dyad] 프로필 사진 그리드 하단 여백 조정

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -409,7 +409,7 @@ const Profile = () => {
               </div>
             </div>
 
-            <div className="flex flex-col -mx-6">
+            <div className="flex flex-col -mx-6 pb-[calc(5.5rem+env(safe-area-inset-bottom,0px))]">
               {/* 저장됨 탭 */}
               <div className={viewMode === 'saved' ? undefined : 'hidden'}>
                 {savedPosts.map((post) => (
