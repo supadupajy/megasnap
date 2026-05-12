ba5a1260e8306a717a74c5e960b52f8cf45c5b5e
[dyad] <supadupajy@gmail.com>
2026-05-12 15:09:41 +0900

    Updated src/pages/Popular.tsx

--- src/pages/Popular.tsx
+++ src/pages/Popular.tsx
@@ -234,7 +234,7 @@ const Popular = () => {
           ActionIcon={Search}
           actionLabel="검색"
           onActionClick={() => window.dispatchEvent(new CustomEvent('open-post-search'))}
-          collapsedHint="Flicks로 전환"
+          collapsedHint="Flicks!"
         />
       </div>
 
