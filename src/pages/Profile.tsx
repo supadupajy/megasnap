e7129bbfb14834f2931560099dab1fbf08351c49
[dyad] <supadupajy@gmail.com>
2026-05-14 01:02:28 +0900

    [dyad] (1 files changed)

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -273,7 +273,7 @@ const Profile = () => {
       if (element && container) {
         const containerTop = container.getBoundingClientRect().top;
         const elementTop = element.getBoundingClientRect().top;
-        const offset = elementTop - containerTop + container.scrollTop - 82;
+        const offset = elementTop - containerTop + container.scrollTop - 60;
         container.scrollTo({ top: offset, behavior: 'smooth' });
       }
     }, 150);
