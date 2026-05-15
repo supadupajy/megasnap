0d745b27191867ed6480b65ff87ff8ba0e9ff525
[dyad] <supadupajy@gmail.com>
2026-05-15 11:25:54 +0900

    [dyad] 하단 네비게이션바 배경 재수정

--- android/app/src/main/java/com/chorasnap/chorasnap/MainActivity.java
+++ android/app/src/main/java/com/chorasnap/chorasnap/MainActivity.java
@@ -1,15 +1,22 @@
 package com.chorasnap.chorasnap;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowInsetsController;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        configureSystemBars();
         super.onCreate(savedInstanceState);
+        configureSystemBars();
 
         // WebView 네이티브 overscroll(rubber-band) 완전 비활성화
         WebView webView = getBridge().getWebView();
@@ -18,9 +25,38 @@ public class MainActivity extends BridgeActivity {
         }
     }
 
+    private void configureSystemBars() {
+        Window window = getWindow();
+        window.setNavigationBarColor(Color.WHITE);
+        window.setStatusBarColor(Color.WHITE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.setNavigationBarContrastEnforced(false);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(true);
+            WindowInsetsController controller = window.getInsetsController();
+            if (controller != null) {
+                controller.setSystemBarsAppearance(
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS |
+                        WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS |
+                        WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+                );
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            );
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+    }
+
     @Override
     public void onStart() {
         super.onStart();
+
         // 웹뷰 설정을 가져와서 사용자 제스처 없이도 미디어 재생(소리 포함)이 가능하도록 설정
         WebView webView = getBridge().getWebView();
         if (webView != null) {
